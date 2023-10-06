b07eb966ada1d49c6df485ead5072798f7514f0f
NNazar6 <144454064+NNazar6@users.noreply.github.com>
2023-10-06 23:31:57 +0300

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,121 @@
+#a = int(input('Input integer number: '))
+#print(a + 1, end='\n')
+#a += 1
+#print(a + 1, end='\n')
+#a += 1
+#print(a + 1, end='\n')
+
+#=============02
+
+#a = int(input(''))
+#b = int(input(''))
+#c = int(input(''))
+#print(a + b + c)
+
+#-----------------03
+
+#a = int(input('Enter a number: '))
+#print('Volum: ', a**3,)
+#print('Square: ', 6*a**2)
+
+#-----------------04
+
+#a = int(input('Enter a number: '))
+#b = int(input('Enter a number: '))
+#f = 3*(a + b) **3 + 275 * b**2 - 127 * a - 41
+#print( f, end='\n')
+
+#-----------------05
+
+#a = int(input('Enter a number: '))
+
+#print('The number following the number', a, ' is:', a+1, 'For the number', a, 'the previous number is:', a-1)
+
+#------------------06
+
+# a = int(input(''))
+# b = int(input(''))
+# c = int(input(''))
+# d = int(input(''))
+# print(a + b + c + d)
+
+#----------------07
+
+# a = int(input('Enter a number: '))
+# b = int(input('Enter a number: '))
+# c = int(input('Enter a number: '))
+#
+# f = a+b*(c-1)
+#
+# print(f, end='\n')
+
+#-----------------08
+
+# a = int(input('Enter a number: '))
+# print(a*2,'---',a*3,'---',a*4,'---',a*5, sep='')
+
+# -------------------09
+
+# a = int(input('Enter a number: '))
+# b = int(input('Enter a number: '))
+# print(b // a)
+# print(b % a)
+
+# -------------------10
+
+# a = int(input('Enter a number: '))
+# print(a - a // 2)
+
+# --------------------11
+
+# a = int(input('Enter a number: '))
+# print(a, 'мин - это', a // 60, 'часа', a%60, 'минут')
+
+# --------------------12
+
+# a = int(input('Enter a number: '))
+#
+# b = a // 100
+# c = a % 100 // 10
+# d = a % 10
+# print( 'Сумма цифр = ', b+c+d, 'Произведение цифр = ', b*c*d )
+
+# ---------------------13
+# a = int(input('Enter a number: '))
+# penguin= '''
+#       _~_
+#      (o o)
+#     /  V  \
+#    /(  _  )\
+#      ^^ ^^
+#      '''
+# print(penguin*a)
+
+# ----------------------14
+
+# a = int(input('Enter a number: '))
+# print(a//10)
+# ---------------------15
+
+# a = int(input('Enter a number: '))
+# print(a//60, a%60)
+#-------------------------16
+
+# a = int(input('Enter a number: '))
+# print(1 - a)
+# -----------------------17
+
+# a = int(input('Enter a number: '))
+# print((a // 2 + 1) * 2)
+# -------------------------18
+
+# v = int(input())
+# t = int(input())
+# print((v * t) % 109)
+
+
+# ------------------------19
+# h = int(input())
+# a = int(input())
+# b = int(input())
+# print((h - a) // (a - b) + 1)
